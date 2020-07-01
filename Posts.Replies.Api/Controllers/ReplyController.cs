232abd747d94b461765c13fd18d979ee738d8d84
Fatih Erol <50841052+dev-fatih-erol@users.noreply.github.com>
2020-07-01 19:44:32 +0300

    .

---
+++ Posts.Replies.Api/Controllers/ReplyController.cs
@@ -0,0 +1,48 @@
+﻿using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Posts.Replies.Application.Commands;
+using Posts.Replies.Application.Queries;
+
+namespace Posts.Replies.Api.Controllers
+{
+    public class ReplyController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public ReplyController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        [Route("Comment/{commentId:length(24)}/Reply")]
+        public async Task<IActionResult> GetByCommentId([FromRoute]string commentId, [FromQuery]int pageIndex = 1)
+        {
+            return Ok(await _mediator.Send(new GetReplyByCommentIdQuery(commentId, pageIndex)));
+        }
+
+        [HttpGet]
+        [Route("Reply/{id:length(24)}")]
+        public async Task<IActionResult> GetById([FromRoute]string id)
+        {
+            var reply = await _mediator.Send(new GetReplyByIdQuery(id));
+
+            if (reply != null)
+            {
+                return Ok(reply);
+            }
+
+            return NotFound();
+        }
+
+        [HttpPost]
+        [Route("Reply")]
+        public async Task<IActionResult> Create([FromBody]CreateReplyCommand command)
+        {
+            var reply = await _mediator.Send(command);
+
+            return Created($"Reply/{reply.Id}", reply);
+        }
+    }
+}
---
+++ Posts.Replies.Api/Startup.cs
@@ -0,0 +1,65 @@
+using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using Posts.Replies.Api.Filters;
+using Posts.Replies.Application.Configurations;
+using Posts.Replies.Application.Validators;
+using Posts.Replies.Infrastructure;
+using Posts.Replies.Infrastructure.Configurations;
+using Posts.Replies.Infrastructure.Services;
+
+namespace Posts.Replies.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<MongoConfiguration>(Configuration.GetSection(nameof(MongoConfiguration)));
+
+            services.AddSingleton<IMongoConfiguration>(o => o.GetRequiredService<IOptions<MongoConfiguration>>().Value);
+
+            services.AddSingleton<ReplyDbContext>();
+
+            services.AddSingleton<IReplyService, ReplyService>();
+
+            services.AddApplication();
+
+            services
+                .AddControllers(o => o.Filters.Add(typeof(GlobalExceptionFilter)))
+                .AddNewtonsoftJson(o => o.SerializerSettings.NullValueHandling = NullValueHandling.Ignore)
+                .AddFluentValidation(o => o.RegisterValidatorsFromAssemblyContaining<CreateReplyValidator>());
+
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
